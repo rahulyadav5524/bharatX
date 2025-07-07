6415da92cf8e1a7281f4b671d90a1a3f798fb26f
Rahul Yadav <rahulyadav5524@gmail.com>
2025-07-07 22:12:56 +0530

    Added demo_search and frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = ()  => {
+  return (
+    <div>
+      <input type="text" placeholder="Enter your name" />
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default App
